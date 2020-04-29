420a71a04de06840225021e7d08376b6f3fd8761
Alexandr Vorontsov <sasha.voroncov.1999@gmail.com>
2020-04-29 15:03:46 +0700

    add chart support

--- release/LakadMatatag.py
+++ release/LakadMatatag.py
@@ -1,7 +1,9 @@
 import sys
 import wave
 
-import GPO.release.programConfig as conf # Здесь лежит путь до файла
+import numpy
+import matplotlib.pyplot as plt
+
 
 
 
@@ -9,7 +11,7 @@ def main():
     #wav = wave.open(conf.file_path + 'test.wav', 'r')
     wav = wave.open(r'../File/test.wav', 'r')
     (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
-    #content = wav.readframes(nframes)
+    content = wav.readframes(nframes)
     wav.close()
     print('nchannels : ', nchannels,
           '\nsampwidth : ',sampwidth,
@@ -17,6 +19,14 @@ def main():
           '\nnframes : ', nframes,
           '\ncomptype : ', comptype,
           '\ncompname : ', compname)
+
+    print(content)
+    samples = numpy.frombuffer(content, dtype=numpy.int16)
+    print(samples)
+
+    plt.plot(samples)
+    plt.ylabel('some numbers')
+    plt.show()
 
     #txt = open('text.txt', 'w')
     #txt.write("lol")
--- sancho/LakadMatatag.py
+++ sancho/LakadMatatag.py
@@ -1,13 +1,17 @@
 import sys
 import wave
 
-import GPO.release.programConfig as conf
+import numpy
+import matplotlib.pyplot as plt
+
+
+
 
 def main():
     #wav = wave.open(conf.file_path + 'test.wav', 'r')
-    wav = wave.open(conf.file_path, 'r')
+    wav = wave.open(r'../File/test.wav', 'r')
     (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
-    #content = wav.readframes(nframes)
+    content = wav.readframes(nframes)
     wav.close()
     print('nchannels : ', nchannels,
           '\nsampwidth : ',sampwidth,
@@ -16,10 +20,19 @@ def main():
           '\ncomptype : ', comptype,
           '\ncompname : ', compname)
 
+    print(content)
+    samples = numpy.frombuffer(content, dtype=numpy.int16)
+    print(samples)
+
+    plt.plot(samples)
+    plt.ylabel('some numbers')
+    plt.show()
+
     #txt = open('text.txt', 'w')
     #txt.write("lol")
     #txt.close()
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
+
